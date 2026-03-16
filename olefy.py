0c26d92527ea18cf18fccf587487295dc0600b05
Klaus Tachtler <klaus@tachtler.net>
2026-03-16 14:30:59 +0100

    Fix regex pattern and update event loop creation
    - Update to python3 (Version 3.14.x)

--- olefy.py
+++ olefy.py
@@ -60,7 +60,7 @@ logger.debug('olefy listen address string: {} (type {})'.format(olefy_listen_add
 if not olefy_listen_addr_string:
     olefy_listen_addr = ""
 else:
-    addr_re = re.compile('[\[" \]]')
+    addr_re = re.compile('r[\[" \]]')
     olefy_listen_addr = addr_re.sub('', olefy_listen_addr_string.replace("'", "")).split(',')
 
 # log runtime variables
@@ -201,7 +201,7 @@ class AIO(asyncio.Protocol):
 
 
 # start the listeners
-loop = asyncio.get_event_loop()
+loop = asyncio.new_event_loop()
 # each client connection will create a new protocol instance
 coro = loop.create_server(AIO, olefy_listen_addr, olefy_listen_port)
 server = loop.run_until_complete(coro)
